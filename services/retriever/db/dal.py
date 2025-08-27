a712519e016164a91117f0e9f600d10adac2d37a
ChaimShvadron <s0527649958@gmail.com>
2025-08-27 13:29:51 +0300

    Refactor fetch_next_batch method to improve query handling and maintainability

--- services/retriever/db/dal.py
+++ services/retriever/db/dal.py
@@ -6,4 +6,21 @@ class MongoDAL:
         collection = self.db[collection_name]
         documents = list(collection.find({}))
         return documents
+
+    def fetch_next_batch(self, collection_name, last_date=None, last_id=None, limit=100):
+
+        collection = self.db[collection_name]
+        query = {}
+        if last_date is not None and last_id is not None:
+            query = {
+                "$or": [
+                    {"createdate": {"$gt": last_date}},
+                    {"createdate": last_date, "_id": {"$gt": last_id}}
+                ]
+            }
+        elif last_date is not None:
+            query = {"createdate": {"$gte": last_date}}
+            
+        cursor = collection.find(query).sort([("createdate", 1), ("_id", 1)]).limit(limit)
+        return list(cursor)
         
