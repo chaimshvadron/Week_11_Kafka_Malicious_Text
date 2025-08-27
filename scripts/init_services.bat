6b5f939551d1209a0fdeb9a892fa98684b046496
ChaimShvadron <s0527649958@gmail.com>
2025-08-27 18:54:55 +0300

    Enhance init_services.bat to include Docker login and update image tagging

--- scripts/init_services.bat
+++ scripts/init_services.bat
@@ -1,2 +1,8 @@
+# Login to Docker
+docker login
+
 # Build the Docker image for the retriever service
-docker build -f services/retriever/Dockerfile -t retriever .
+docker build -f services/retriever/Dockerfile -t chaimshvadron/retriever:latest .
+
+# Push the Docker image to the repository
+docker push chaimshvadron/retriever:latest
