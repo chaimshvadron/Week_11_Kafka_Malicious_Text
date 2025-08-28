9d39177ae6f9e7bbd00ef478a4d83c98f5c30e50
ChaimShvadron <s0527649958@gmail.com>
2025-08-28 16:57:15 +0300

    Add Dockerfiles for enricher and preprocessor services
    - Created Dockerfile for the enricher service, setting up the environment and dependencies.
    - Created Dockerfile for the preprocessor service, establishing the necessary environment and installing required packages.
    - Both Dockerfiles utilize Python 3.11-slim and configure the working directory, environment variables, and command to run the respective services.

--- shared/kafka/consumer.py
+++ shared/kafka/consumer.py
@@ -3,7 +3,7 @@ import os
 import json
 
 def get_consumer(topics, group_id="default-group"):
-    kafka_server = os.getenv("KAFKA_SERVERS", "localhost:9092")
+    kafka_server = os.getenv("KAFKA_SERVERS")
     return KafkaConsumer(
         *topics,
         bootstrap_servers=kafka_server,
--- shared/kafka/kafka_configurations.py
+++ shared/kafka/kafka_configurations.py
@@ -3,7 +3,7 @@ import os
 import json
 
 def get_producer_config():
-    kafka_server = os.getenv("KAFKA_SERVERS", "localhost:9092")
+    kafka_server = os.getenv("KAFKA_SERVERS")
     return KafkaProducer(
         bootstrap_servers=kafka_server,
         value_serializer=lambda v: json.dumps(v, default=str).encode('utf-8')
