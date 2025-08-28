2af5eddaed7874e04fe5fbb2d816229fab9c39f8
davidkupferstin <k6780519@gmail.com>
2025-08-28 10:58:12 +0300

    An update to the WeaponProcessor that will both clean up the weapons and cause the department to return a list of all weapons, not just one.

--- services/enricher/weapon_processor.py
+++ services/enricher/weapon_processor.py
@@ -1,16 +1,21 @@
+from shared.text_processing.text_processor import TextProcessor
+
 class WeaponProcessor:
     def __init__(self, weapon_list_path):
-        weapon_list = []
+        processor = TextProcessor()
+        self.weapon_list = []
         with open(weapon_list_path, encoding='utf-8') as f:
-            for line in f:
-                weapon = line.strip()
-                if weapon:
-                    weapon_list.append(weapon)
-        self.weapon_list = weapon_list
+            for weapon in f:
+                clean_weapon = processor.process(weapon)
+                if clean_weapon:
+                    self.weapon_list.append(clean_weapon)
 
     def find_weapon(self, text):
+        weapon_references = []
         for weapon in self.weapon_list:
             if weapon in text:
                 print(f"Found weapon: {weapon}")
-                return weapon
+                weapon_references.append(weapon)
+        if weapon_references:
+            return weapon_references
         return None
